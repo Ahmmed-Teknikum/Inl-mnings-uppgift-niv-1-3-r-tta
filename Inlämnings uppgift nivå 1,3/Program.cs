81723f2a34a2cc0e713fdaface27bb4c850080e1
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-13 08:57:03 +0100

    Add project files.

---
+++ Inlämnings uppgift nivå 1,3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace inlämningUppgift_._nivå1_._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int totalsum = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Ange heltal:");
+                int nummer = int.Parse(Console.ReadLine());
+                totalsum += nummer;
+
+            }
+            Console.WriteLine("Summan av alla tal är:" + totalsum);
+
+
+        }
+    }
+}
